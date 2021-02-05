8410fe554f1ddcf8f37936cc51abf134cdef7f30
Javier Botero <javierbotero10271@gmail.com>
2021-02-05 17:53:39 -0500

    Sets the main actions in Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,39 @@
 class UsersController < ApplicationController
   def show
     @user = User.where('email = ? AND password = ?', params[:email], params[:password])
+
+    if @user
+      render json: @user
+    else
+      render nothing: true, status: 404
+    end
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+    else
+      render nothing: true, status: 404
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update!(user_params)
+      render json: @user
+    else
+      render nothing: true, status: 404
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    render json: { result: 'User deleted' }
   end
 
   private
