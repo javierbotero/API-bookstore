6a0dc6e5329d519dbead6aa4dde228b00f21889e
Javier Botero <javierbotero10271@gmail.com>
2021-02-19 18:12:00 -0500

    Adds validation to model Book to ensure a correct percentage amount

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,5 @@ class Book < ApplicationRecord
 
   validates :title, :category, :completed, :author, :completed, presence: true
   validates :title, :author, length: { in: 1..100 }
+  validates :completed, inclusion: { in: 0..100, message: "%{value} is not a valid percentage size for your reading completion" }
 end
